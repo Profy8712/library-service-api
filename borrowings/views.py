5c65fec39ec9da54d28888f399ff077d17e67e27
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-04 09:56:46 +0300

    Implement create endpoint

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,17 +1,35 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingReadSerializer
+from borrowings.serializers import BorrowingReadSerializer, BorrowingCreateSerializer
 
 
-class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+class BorrowingViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Borrowing.objects.select_related("book", "user")
-    serializer_class = BorrowingReadSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        queryset = self.queryset
-        if not self.request.user.is_staff:
-            queryset = queryset.filter(user=self.request.user)
-        return queryset
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingReadSerializer
+
+    def perform_create(self, serializer):
+        book = serializer.validated_data["book"]
+        book.inventory -= 1
+        book.save()
+        serializer.save(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        read_serializer = BorrowingReadSerializer(instance=serializer.instance)
+        return Response(read_serializer.data, status=status.HTTP_201_CREATED)
