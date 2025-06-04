ac19742b5724c94ffd1172c02bbe89ead194928b
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-04 09:34:53 +0300

    Implement BorrowingReadSerializer

---
+++ borrowings/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from books.serializers import BookSerializer
+from users.serializers import UserSerializer
+
+from borrowings.models import Borrowing
+
+
+class BorrowingReadSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        ]
